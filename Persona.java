e699cf1d49fdfd9b31ce29533562d8573866c1af
Juan <jgodoy@gmail.com>
2022-02-18 13:38:49 -0600

    Test 2

---
+++ Persona.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Laboratorio5;
+
+/**
+ *
+ * @author jcgof
+ */
+public class Persona {
+    String nombre,
+    
+    
+    
+}
